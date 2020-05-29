940ed2da283a4e4e4cf9c5a13a61d32331e7f327
Gitea <gitea@fake.local>
2020-05-29 14:43:12 +0500

    Get-AliasesToExport - fix.

--- Functions/Private/Get-AliasesToExport.ps1
+++ Functions/Private/Get-AliasesToExport.ps1
@@ -14,17 +14,24 @@ function Get-AliasesToExport {
     )
     [string]$theFName = "[$($MyInvocation.MyCommand.Name)]:"
     $aliasesToExport = @()
-    [System.IO.FileInfo[]]$scriptsAll = Get-ChildItem -Path $Path -File -Filter '*.ps1'
+    [System.IO.FileInfo[]]$scriptsAll = Get-ChildItem -Path $Path -File -Filter '*.ps1' -Recurse
+    Write-Verbose -Message "$theFName Found $($scriptsAll.Count) scripts in path `"$Path`""
     [System.IO.FileInfo[]]$scriptsFunc = $scriptsAll.Where({$_.BaseName -in $Functions})
+    Write-Verbose -Message "$theFName Found $($scriptsFunc.Count) functions in path `"$Path`""
     $scriptsFunc.ForEach({
-        if (Get-Command -Name $_.FullName) {
-            try {
-                $aliasesToExport += (Get-Alias -Definition $_.BaseName)
-            } catch {
-                Write-Warning -Message "$theFName Function `"$($_.BaseName)`" has no aliases!"
-            }
-        } else {
-            Write-Warning -Message "$theFName Cannot get command `"$($_.BaseName)`"!"
+        [string]$scriptFullName = $_.FullName
+        [string]$scriptBaseName = $_.BaseName
+        try {
+            Get-Command -Name $scriptBaseName -ErrorAction Stop
+        }
+        catch {
+            Write-Verbose -Message "$theFName Function `"$scriptBaseName`" is not imported. Dot sourcing it from path `"$scriptFullName`"..."
+            . $scriptFullName
+        }
+        try {
+            $aliasesToExport += (Get-Alias -Definition $scriptBaseName -ErrorAction Stop)
+        } catch {
+            Write-Warning -Message "$theFName Function `"$scriptBaseName`" has no aliases!"
         }
     })
     return $aliasesToExport
